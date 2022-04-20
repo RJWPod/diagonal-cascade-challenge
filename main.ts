7fe704e83f2189b0283077f5569e8138fd787ec3
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-04-20 09:56:27 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    for (let x = 0; x <= 4; x++) {
+        led.plot(x, x)
+        for (let y = 0; y <= 4; y++) {
+        	
+        }
+    }
+})
+basic.forever(function () {
+	
+})
