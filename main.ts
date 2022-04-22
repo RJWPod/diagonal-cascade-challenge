1e1df8b6d9881c733c80749ca30516d0bfbf9ed4
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-04-22 09:39:46 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    for (let x = 0; x <= 4; x++) {
-        for (let y = 0; y <= 4; y++) {
-            led.toggle(x, x)
-            led.toggle(x, 0)
-            basic.pause(50)
+    for (let x = 0; x <= 8; x++) {
+        for (let y = 0; y <= 8; y++) {
+            led.toggle(x - y, y)
+            basic.pause(10)
         }
     }
 })
 input.onButtonPressed(Button.B, function () {
-    for (let x = 0; x <= 4; x++) {
-        for (let y = 0; y <= 4; y++) {
-        	
+    for (let x = 0; x <= -8; x++) {
+        for (let y = 0; y <= -8; y++) {
+            led.toggle(x - y, y)
+            basic.pause(10)
         }
     }
 })
